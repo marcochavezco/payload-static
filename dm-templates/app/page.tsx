bdace51b7360cd3889d78d6aef8f011257fd02eb
marcochavezco <machavezco@gmail.com>
2025-03-05 10:31:19 -0600

    organize files

---
+++ dm-templates/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div
+      id='safe-area'
+      className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'
+    >
+      <h1 className='text-4xl font-bold text-center'>Hello</h1>
+      <p className='text-lg text-center'>Test</p>
+      <footer className='text-sm text-center'></footer>
+    </div>
+  );
+}
